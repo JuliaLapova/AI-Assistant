4901657f2217c2ca9349b4752c2e4bd078b0f1a3
JuliaLapova <119416226+JuliaLapova@users.noreply.github.com>
2023-08-27 03:13:19 +0300

    Update __init__.py

--- fastapi_app/chatbot/__init__.py
+++ fastapi_app/chatbot/__init__.py
@@ -1,4 +1,4 @@
 import openai
 
-OPENAI_API_KEY = "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"  #@param {type: "string"}
+OPENAI_API_KEY = "your_key"  #@param {type: "string"}
 openai.api_key = OPENAI_API_KEY
