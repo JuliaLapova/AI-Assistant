3e0eb57f8830e69b425a96b7ef86bc09d834de33
JuliaLapova <119416226+JuliaLapova@users.noreply.github.com>
2023-08-27 03:20:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,44 +4,9 @@ from fastapi_app.routes.api_routes import get_answer_with_sources
 import os
 import requests
 
-##@st.cache(allow_output_mutation=True)
-#@st.cache_data()  # Streamlit recommends to use this method
-##@st.cache_resource(allow_output_mutation=True)  # Streamlit recommends to use this method
-#def get_data_file(url):
-##    localpath = "/tmp/tempfile"
-#    localpath = "/tmp/index.faiss"
-#    r = requests.get(url)
-#    with open(localpath, 'wb') as f:
-#        f.write(r.content)
-#    return localpath
-
-##def main():
-#file_url = 'https://github.com/JuliaLapova/AI-Assistant/raw/main/faiss/faiss-hr/index.faiss'
-##    file_url = 'https://github.com/JuliaLapova/AI-Assistant/raw/main/faiss/faiss-hr/index.faiss'
-#localfile = get_data_file(file_url)
-
-#    # Дальше вы можете использовать localfile в вашем приложении, как если бы это был локальный файл
-
-##if __name__ == "__main__":
-##    main()
-
-#    # Выводим путь к файлу
-#st.write(f'localfile: {localfile}')
-
-#    # Проверяем наличие файла и выводим результат проверки
-#st.write(f'Проверяем наличие файла: {localfile}')    
-#st.write(os.path.exists(localfile))
-
-
-#dir_path = '/tmp/'
-#dir_contents = os.listdir(dir_path)
-
-#st.write(dir_contents)
-
 col1, col2 = st.columns(2)
 
 with col1:
-#    st.title('Привет, я ваш умный помощник!')
     st.markdown("<h2>Привет, я ваш умный помощник!</h2>", unsafe_allow_html=True)
     st.write('Интеллектуальный консультант для сотрудников СПб ГКУ «МФЦ»')
 
@@ -52,17 +17,10 @@ with col2:
         </div>
         """, unsafe_allow_html=True)
 
-
-
-#with col2:
-#    st.image('https://drive.google.com/uc?export=view&id=1g0KmrIILmwb0Vrxt-J-n0HFvQBDhk0Lr')
-
-
 # Создаем форму для ввода данных пользователем
 with st.form(key='my_form'):
     text_input = st.text_input(label='Введите текст')
     submit_button = st.form_submit_button(label='Отправить')
-
 
 # Выводим данные, введенные пользователем
 #if text_input:
@@ -76,17 +34,7 @@ if text_input:  # Если text_input не пустой и не None
   api_key = "your_key"
   topic = "main" # yt
   translate_answer = False # передайте True, если вы хотите переводить ответ
-  #answer, sources = get_answer_with_sources(user_input, api_key, topic, translate_answer)
   answer, sources = get_answer_with_sources(user_input, api_key, topic, translate_answer)
-
-
-# Выводим данные, введенные пользователем
-#if text_input:
-#    st.write(f'Вы ввели: {text_input}')
-#    answer = get_answer_with_sources(document = text_input)
-#    st.write(f'Ваш ответ:\n {answer}')
-#    st.write(f'Источники: {sources}')
-#    
 
 if text_input:
     st.write(f'Ваш вопрос: {text_input}')
